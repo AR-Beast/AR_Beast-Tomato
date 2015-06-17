ac8e0a4495c52fdeb88a6284c5fd5f831e02b3dd
AayushRd7 <aayush835@gmail.com>
2015-06-17 10:14:44 +0530

    Add Support for UKSM | Ultra Kernel Samepage Merging
    Credits  AayushRd7

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -742,6 +742,11 @@ const char * const vmstat_text[] = {
 #endif
 	"nr_anon_transparent_hugepages",
 	"nr_free_cma",
+	"nr_swapcache",
+
+#ifdef CONFIG_UKSM
+	"nr_uksm_zero_pages",
+#endif
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
 
