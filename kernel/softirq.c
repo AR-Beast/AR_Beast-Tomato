7857a72f4fa15b79afb663ccaf00070ff8aa13af
Shoaib0597 <Shoaib0595@gmail.com>
2015-11-20 20:47:47 +0530

    Reduced CPU Load
    Conflicts:
    	kernel/softirq.c

--- kernel/softirq.c
+++ kernel/softirq.c
@@ -645,8 +645,8 @@ static void run_ksoftirqd(unsigned int cpu)
 		cond_resched();
 
 		preempt_disable();
-		rcu_note_context_switch(cpu);
-		preempt_enable();
+ 		rcu_note_context_switch(cpu);
+ 		preempt_enable();
 
 		return;
 	}
