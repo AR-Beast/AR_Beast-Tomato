5cbc3ea6e43dd9bd251f8b5bc8ceb437a92ee9aa
Shoaib0597 <Shoaib0595@gmail.com>
2016-06-09 13:54:36 +0530

    Underclock GPU to 50MHz

--- drivers/clk/qcom/clock-gcc-8936.c
+++ drivers/clk/qcom/clock-gcc-8936.c
@@ -851,7 +851,7 @@ static struct rcg_clk gfx3d_clk_src = {
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP4(LOW, 160000000, NOMINAL, 310000000, NOMINAL_PLUS, 400000000, HIGH, 550000000),
+		VDD_DIG_FMAX_MAP4(LOW, 50000000, NOMINAL, 310000000, NOMINAL_PLUS, 400000000, HIGH, 550000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
