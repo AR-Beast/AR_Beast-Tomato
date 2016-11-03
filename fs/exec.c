25d7135a3c41de71df7811db85ae71b66c6a0a82
Joe Maples <frap129@gmail.com>
2016-11-03 09:39:11 -0400

    uksm: fx/exec: Remove unused include Conflicts: fs/exec.c
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -55,7 +55,7 @@
 #include <linux/pipe_fs_i.h>
 #include <linux/oom.h>
 #include <linux/compat.h>
-#include <linux/ksm.h>
+#include <linux/resource.h>
 
 #include <asm/uaccess.h>
 #include <asm/mmu_context.h>
