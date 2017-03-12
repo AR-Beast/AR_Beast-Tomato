b3179be5d1c22b5002180aa96bf2f4aedaefb835
Ayush <ayushrathore12501@gmail.com>
2017-03-12 12:21:08 +0530

    Increase Charge frequency

--- drivers/power/thundercharge_control.c
+++ drivers/power/thundercharge_control.c
@@ -20,7 +20,7 @@
 #include "thundercharge_control.h"
 
 #define ENABLED             1
-#define AC_CURRENT          1250
+#define AC_CURRENT          1370
 #define USB_CURRENT         1000
 #define MAX_VBUS_CURRENT    1500
 #define THUNDERCHARGE       "thundercharge"
