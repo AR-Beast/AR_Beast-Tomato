8e6ddcd7b60abc1eb19b1c14a6d8a483f5e0c1cf
AyushR1 <ayushrathore12501@gmail.com>
2017-06-16 19:43:18 +0530

    Revert back to stock GPU freq
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/clk/qcom/clock-gcc-8936.c
+++ drivers/clk/qcom/clock-gcc-8936.c
@@ -838,7 +838,7 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
 	F( 310000000,	gpll2_gfx3d,	3,	  0,	0),
 	F( 400000000,      gpll0_out_main,   2,	  0,	0),
 	F( 465000000,      gpll2_gfx3d,   2,    0,    0),
-	F( 650000000,      gpll3_out_main,   2,    0,    0),
+	F( 550000000,      gpll3_out_main,   2,    0,    0),
 	F_END
 };
 
@@ -851,7 +851,8 @@ static struct rcg_clk gfx3d_clk_src = {
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP4(LOW, 19200000, NOMINAL, 310000000, NOMINAL_PLUS, 400000000, HIGH, 650000000),
+		VDD_DIG_FMAX_MAP4(LOW, 220000000, NOMINAL, 400000000, NOMINAL_PLUS, 465000000, HIGH,
+			550000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
