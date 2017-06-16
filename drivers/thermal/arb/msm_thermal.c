623359fed7d15fd16d5ca7a1f9e4463336e0ae33
AyushR1 <ayushrathore12501@gmail.com>
2017-06-16 14:21:41 +0530

    Thermal: Store Sysfs nodes Properly and do Sanity checks.
    Thanks to @Yoinx
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/thermal/arb/msm_thermal.c
+++ drivers/thermal/arb/msm_thermal.c
@@ -1,5 +1,7 @@
 /* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
- *
+ * Copyright (c) 2015 Francisco Franco
+ * Copyright (c) 2017 Ayush Rathore
+ * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
  * only version 2 as published by the Free Software Foundation.
@@ -38,14 +40,10 @@
 #include <linux/regulator/rpm-smd-regulator.h>
 #include <linux/regulator/consumer.h>
 #include <linux/regulator/driver.h>
-#include <linux/msm_thermal_ioctl.h>
 #include <soc/qcom/rpm-smd.h>
 #include <soc/qcom/scm.h>
 #include <linux/sched/rt.h>
 #include <linux/ratelimit.h>
-
-#define CREATE_TRACE_POINTS
-#define TRACE_MSM_THERMAL
 #include <trace/trace_thermal.h>
 
 #define MAX_CURRENT_UA 100000
@@ -57,10 +55,10 @@
 #define THERM_SECURE_BITE_CMD 8
 #define SENSOR_SCALING_FACTOR 1
 #define CPU_DEVICE "cpu%d"
-
-/* Throttle CPU when it reaches a certain temp*/
-unsigned int temp_threshold = 50;
-module_param(temp_threshold, int, 0644);
+#define CREATE_TRACE_POINTS
+#define TRACE_MSM_THERMAL
+#define _temp_threshold		50
+#define _temp_step	3
 
 static struct thermal_info {
 	uint32_t cpuinfo_max_freq;
@@ -69,72 +67,131 @@ static struct thermal_info {
 	bool throttling;
 	bool pending_change;
 } info = {
-	.cpuinfo_max_freq = LONG_MAX,
-	.limited_max_freq = LONG_MAX,
+	.cpuinfo_max_freq = UINT_MAX,
+	.limited_max_freq = UINT_MAX,
 	.safe_diff = 5,
 	.throttling = false,
 	.pending_change = false,
 };
 
-/* Throttle Freq in MHz */
-unsigned int FREQ_ZONEH	= 200000;
-module_param(FREQ_ZONEH, int, 0644);
+int ENABLED = 1;
+int TEMP_THRESHOLD = _temp_threshold;
+int TEMP_STEP = _temp_step;
+int LEVEL_VERY_HOT = _temp_threshold + _temp_step;
+int LEVEL_HOT = _temp_threshold + (_temp_step * 2);
+int LEVEL_HELL = _temp_threshold + (_temp_step * 3);
+int FREQ_HELL = 1113600;
+int FREQ_VERY_HOT = 1344000;
+int FREQ_HOT = 1459200;
+int FREQ_WARM = 1574000;
+int FREQ_MAX = 1708000;
 
-unsigned int FREQ_ZONEG	= 345600;
-module_param(FREQ_ZONEG, int, 0644);
+static int set_temp_threshold(const char *val, const struct kernel_param *kp)
+{
+	int ret = 0;
+	int i;
 
-unsigned int FREQ_ZONEF	= 533333;
-module_param(FREQ_ZONEF, int, 0644);
+	ret = kstrtouint(val, 10, &i);
+	if (ret)
+		return -EINVAL;
+	if (i < 20 || i > 100)
+		return -EINVAL;
+	
+	LEVEL_VERY_HOT = i + TEMP_STEP;
+	LEVEL_HOT = i + (TEMP_STEP * 2);
+	LEVEL_HELL = i + (TEMP_STEP * 3);
+	
+	ret = param_set_int(val, kp);
 
-unsigned int FREQ_ZONEE	= 800000;
-module_param(FREQ_ZONEE, int, 0644);
+	return ret;
+}
 
-unsigned int FREQ_ZONED	= 1113600;
-module_param(FREQ_ZONED, int, 0644);
+static struct kernel_param_ops temp_threshold_ops = {
+	.set = set_temp_threshold,
+	.get = param_get_int,
+};
 
-unsigned int FREQ_ZONEC	= 1344000;
-module_param(FREQ_ZONEC, int, 0644);
+module_param_cb(temp_threshold, &temp_threshold_ops, &TEMP_THRESHOLD, 0644);
 
-unsigned int FREQ_ZONEB	= 1459200;
-module_param(FREQ_ZONEB, int, 0644);
+static int set_temp_step(const char *val, const struct kernel_param *kp)
+{
+	int ret = 0;
+	int i;
 
-unsigned int FREQ_ZONEA	= 1574000;
-module_param(FREQ_ZONEA, int, 0644);
- 
-unsigned int FREQ_ZONE = 1708000;
-module_param(FREQ_ZONE, int, 0644);
+	ret = kstrtouint(val, 10, &i);
+	if (ret)
+		return -EINVAL;
+	/*	Restrict the values to 1-6 as this will result in threshold + value - value *3
+		without a restriction this could result in significanty higher than expected values*/
+	if (i < 1 || i > 6)
+		return -EINVAL;
+	
+	LEVEL_VERY_HOT = TEMP_THRESHOLD + i;
+	LEVEL_HOT = TEMP_THRESHOLD + (i * 2);
+	LEVEL_HELL = TEMP_THRESHOLD + (i * 3);
+	
+	ret = param_set_int(val, kp);
 
+	return ret;
+}
 
-/* Temp Diferrence */
-unsigned int temp_step = 3;
-module_param(temp_step, int, 0644);
+static struct kernel_param_ops temp_step_ops = {
+	.set = set_temp_step,
+	.get = param_get_int,
+};
 
-/* Core control temp */
-int corecontrol = 60;
+module_param_cb(temp_step, &temp_step_ops, &TEMP_STEP, 0644);
 
-/* CPU Hotplugging Switch */
-unsigned int temp_safety = 1;
-module_param(temp_safety, int, 0644);
+static int set_freq_limit(const char *val, const struct kernel_param *kp)
+{
+	int ret = 0;
+	int i, cnt;
+	int valid = 0;
+	struct cpufreq_policy *policy;
+	static struct cpufreq_frequency_table *tbl = NULL;
+	
+	ret = kstrtouint(val, 10, &i);
+	if (ret)
+		return -EINVAL;
 
-/* Extras */
-unsigned int enabled = 0;
-module_param(enabled, int, 0664);
+	policy = cpufreq_cpu_get(0);
+	tbl = cpufreq_frequency_get_table(0);
+	for (cnt = 0; (tbl[cnt].frequency != CPUFREQ_TABLE_END); cnt++) {
+		if (cnt > 0)
+			if (tbl[cnt].frequency == i)
+				valid = 1;
+	}
+			
+	if (strcmp( kp->name, "msm_thermal.freq_warm") == 0 && i <= FREQ_HOT) 
+		return -EINVAL;
+	if ( strcmp( kp->name, "msm_thermal.freq_hot") == 0 &&  ( i >= FREQ_WARM || i <= FREQ_VERY_HOT ))
+		return -EINVAL;	
+	if ( strcmp( kp->name, "msm_thermal.freq_very_hot") == 0 && ( i >= FREQ_HOT || i <= FREQ_HELL ))
+		return -EINVAL;		
+	if ( strcmp( kp->name, "msm_thermal.freq_hell") == 0 && i >= FREQ_VERY_HOT ) 
+		return -EINVAL;		
+
+	if (!valid)
+		return -EINVAL;
+	
+	ret = param_set_int(val, kp);
+
+	return ret;
+}
+
+static struct kernel_param_ops freq_limit_ops = {
+	.set = set_freq_limit,
+	.get = param_get_int,
+};
+
+module_param_cb(freq_hell, &freq_limit_ops, &FREQ_HELL, 0644);
+module_param_cb(freq_very_hot, &freq_limit_ops, &FREQ_VERY_HOT, 0644);
+module_param_cb(freq_hot, &freq_limit_ops, &FREQ_HOT, 0644);
+module_param_cb(freq_warm, &freq_limit_ops, &FREQ_WARM, 0644);
 
 static struct msm_thermal_data msm_thermal_info;
 static struct delayed_work check_temp_work;
 static struct workqueue_struct *thermal_wq;
-
-static void cpu_offline_wrapper(int cpu)
-{
-        if (cpu_online(cpu))
-			cpu_down(cpu);
-}
-
-static void __ref cpu_online_wrapper(int cpu)
-{
-        if (!cpu_online(cpu))
-			cpu_up(cpu);
-}
 
 static int msm_thermal_cpufreq_callback(struct notifier_block *nfb,
 		unsigned long event, void *data)
@@ -187,31 +244,23 @@ static void check_temp(struct work_struct *work)
 	tsens_get_temp(&tsens_dev, &temp);
 
 	if (info.throttling) {
-		if (temp < (temp_threshold - info.safe_diff)) {
+		if (temp < (TEMP_THRESHOLD - info.safe_diff)) {
 			limit_cpu_freqs(info.cpuinfo_max_freq);
 			info.throttling = false;
 			goto reschedule;
 		}
 	}
 
-	if (temp >= temp_threshold + (temp_step * 7))
-		freq = FREQ_ZONEH;
-	else if (temp >= temp_threshold + (temp_step * 6))
-		freq = FREQ_ZONEG;
-	else if (temp >= temp_threshold + (temp_step * 5))
-		freq = FREQ_ZONEF;
-	else if (temp >= temp_threshold + (temp_step * 4))
-		freq = FREQ_ZONEE;
-	else if (temp >= temp_threshold + (temp_step * 3))
-		freq = FREQ_ZONED;
-	else if (temp >= temp_threshold + (temp_step * 2))
-		freq = FREQ_ZONEC;
-	else if (temp >= temp_threshold + temp_step)
-		freq = FREQ_ZONEB;
-	else if (temp >= temp_threshold)
-		freq = FREQ_ZONEA;
-	else if (temp < temp_threshold)
-		freq = FREQ_ZONE;
+    if (temp >= LEVEL_HELL)
+		freq = FREQ_HELL;
+	else if (temp >= LEVEL_VERY_HOT)
+		freq = FREQ_VERY_HOT;
+	else if (temp >= LEVEL_HOT)
+		freq = FREQ_HOT;
+	else if (temp >= TEMP_THRESHOLD)
+		freq = FREQ_WARM;
+	else if (temp < TEMP_THRESHOLD)
+		freq = FREQ_MAX;
 
 	if (freq) {
 		limit_cpu_freqs(freq);
@@ -219,46 +268,6 @@ static void check_temp(struct work_struct *work)
 		if (!info.throttling)
 			info.throttling = true;
 	}
-
-/* CPU UNplugging */
-if(temp_safety==1){
-	if (temp >= (corecontrol)){
-	    cpu_offline_wrapper(1);
-		cpu_offline_wrapper(2);
-		cpu_offline_wrapper(3);}
-	if (temp >= (corecontrol + 6)){
-	    cpu_offline_wrapper(4);
-		cpu_offline_wrapper(5);}
-}
-
-
-/* CPU Plugging */
-if(temp_safety==1){
-	if (temp < (corecontrol)){
-	    cpu_online_wrapper(1);
-		cpu_online_wrapper(2);
-		cpu_online_wrapper(3);}
-    if (temp < (corecontrol + 6)){
-	    cpu_online_wrapper(4);
-		cpu_online_wrapper(5);
-		cpu_online_wrapper(6);
-		cpu_online_wrapper(7);}
-}
-
-
-/* Quad core Mode */
-if(enabled==1){
-		cpu_online_wrapper(1);
-		if(temp_safety==1){
-	    if (temp > (corecontrol)){
-		cpu_offline_wrapper(1);}}
-	    cpu_offline_wrapper(2);
-		cpu_offline_wrapper(3);
-	    cpu_offline_wrapper(6);
-		cpu_offline_wrapper(7);
-		cpu_online_wrapper(4);
-		cpu_online_wrapper(5);}
-
 
 reschedule:
 	queue_delayed_work(system_power_efficient_wq, &check_temp_work, msecs_to_jiffies(250));
