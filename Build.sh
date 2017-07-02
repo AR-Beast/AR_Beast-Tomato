206eda6e5b26efaf21399987b59542acc21b382f
AyushR1 <ayushrathore12501@gmail.com>
2017-07-02 17:41:55 +0530

    Fixes.
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- Build.sh
+++ Build.sh
@@ -45,6 +45,7 @@ echo -e "***********************************************$nocol"
 rm -f $KERN_IMG
 make ARBeast_tomato_defconfig -j4
 make Image -j4
+make dtbs -j4
 make modules -j4
 if ! [ -a $KERN_IMG ];
 then
