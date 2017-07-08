301940bc8544d5881cef2f83ae23573363534a98
Shoaib0597 <Shoaib0595@gmail.com>
2017-07-08 17:06:42 +0530

    Introduce Quick Charge+
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    This Driver acts as a Current (mA) Booster for Devices which already Support Qualcomm's Quick Charge Technology. This Driver, called as Quick Charge+, simply Relaxes the Input-Current (mA) Limits as well as Increases the Pre-Set Charging-Current (mA) Levels. As a result, the Charging Speed becomes Quite Faster.
    
    This Driver is still a Work-in-Progress and I am Testing the Safety and Worthiness at the moment.
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

---
+++ drivers/power/Quick_Charge.c
@@ -0,0 +1,116 @@
+/* Quick Charge+, a Current-Booster Driver for Qualcomm's Quick-Charge Technology.
+ *
+ * Copyright (c) 2017, Shoaib Anwar <Shoaib0595@gmail.com>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/Quick_Charge.h>
+
+#define ENABLED             0
+#define QUICK_CHARGE        "Quick_Charge"
+
+// Enable/Disable Toggle.
+int QC_Toggle = ENABLED;
+// Variable to Store Actual Value of Current (mA) Drawn from Charger.
+unsigned int Dynamic_Current;
+
+// Function to Read the Value of Current (mA) Drawn from Charger.
+void Current (int Value)
+{
+Dynamic_Current = Value / 1000;
+}
+
+static ssize_t qc_toggle_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d", QC_Toggle);
+}
+
+static ssize_t qc_toggle_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+	int val;
+	sscanf (buf, "%d", &val);
+	switch (val)
+	{
+	case 0:
+	       QC_Toggle = val;
+	break;
+	case 1:
+	       QC_Toggle = val;
+	break;
+	default:
+		pr_info("%s: Invalid Value", QUICK_CHARGE);
+    	break;
+	}
+return count;
+}
+
+static ssize_t dynamic_current_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%u", Dynamic_Current);
+}
+
+static struct kobj_attribute qc_toggle_attribute =
+	__ATTR(QC_Toggle,
+		0666,
+		qc_toggle_show,
+		qc_toggle_store);
+
+static struct kobj_attribute dynamic_current_attribute =
+	__ATTR(Dynamic_Current,
+		0444,
+		dynamic_current_show, 
+		NULL);
+
+static struct attribute *charger_control_attrs[] =
+	{
+		&qc_toggle_attribute.attr,
+		&dynamic_current_attribute.attr,
+		NULL,
+	};
+	
+static struct attribute_group chgr_control_attr_group =
+	{
+		.attrs = charger_control_attrs,
+	};
+
+static struct kobject *charger_control_kobj;
+
+static int charger_control_probe(void)
+{
+	int sysfs_result;
+	printk(KERN_DEBUG "[%s]\n",__func__);
+
+	charger_control_kobj = kobject_create_and_add("Quick_Charge", kernel_kobj);
+
+	if (!charger_control_kobj) 
+	{
+	   pr_err("%s Interface create failed!\n", __FUNCTION__);
+	   return -ENOMEM;
+        }
+
+	sysfs_result = sysfs_create_group(charger_control_kobj, &chgr_control_attr_group);
+
+	if (sysfs_result) 
+	{
+	   pr_info("%s sysfs create failed!\n", __FUNCTION__);
+	   kobject_put(charger_control_kobj);
+	}
+	return sysfs_result;
+}
+
+static void charger_control_remove(void)
+{
+	if (charger_control_kobj != NULL)
+	   kobject_put(charger_control_kobj);
+}
+
+module_init(charger_control_probe);
+module_exit(charger_control_remove);
+MODULE_LICENSE("GPL and Additional Rights");
+MODULE_AUTHOR("Shoaib Anwar <Shoaib0595@gmail.com>");
+MODULE_DESCRIPTION("Quick Charge+");
---
+++ include/linux/Quick_Charge.h
@@ -0,0 +1,18 @@
+/* Header File for Quick Charge+ Driver.
+ *
+ * Copyright (c) 2017, Shoaib Anwar <Shoaib0595@gmail.com>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ */
+
+#ifndef __LINUX_QUICK_CHARGE_H
+#define __LINUX_QUICK_CHARGE_H
+
+extern int QC_Toggle;
+void Current (int);
+		  
+#endif
+
