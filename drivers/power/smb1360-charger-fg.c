acbfe036bd6d52c2ee9256fce8c5c7e6278e0b81
AyushR1 <ayushrathore12501@gmail.com>
2017-08-01 14:13:24 +0530

    prima: update to LA.BR.1.2.9_rb1.23
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/power/smb1360-charger-fg.c
+++ drivers/power/smb1360-charger-fg.c
@@ -5424,6 +5424,7 @@ fail_hw_init:
 static int smb1360_remove(struct i2c_client *client)
 {
 	struct smb1360_chip *chip = i2c_get_clientdata(client);
+
 	regulator_unregister(chip->otg_vreg.rdev);
 	power_supply_unregister(&chip->batt_psy);
 	mutex_destroy(&chip->charging_disable_lock);
