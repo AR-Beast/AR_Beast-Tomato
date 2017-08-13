a3d502363af2f54a2e53fa88cc3e516d55a01937
AyushR1 <ayushrathore12501@gmail.com>
2017-08-13 17:35:19 +0530

    This would not cause troubles again.
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- Build.sh
+++ Build.sh
@@ -89,6 +89,7 @@ cp $KERNEL_DIR/spectrum/init.spectrum.rc $ZIP_MAKER_DIR/ramdisk/
 
 echo "**** Time to zip up! ****"
 cd $ZIP_MAKER_DIR/
+git checkout lettuce
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
 rm -rf /home/beast12/ARBeast/$FINAL_KERNEL_ZIP
 cp /home/ayushr1/AR_Beast/ARB-Anykernel2/$FINAL_KERNEL_ZIP /home/ayushr1/AR_Beast/out/lettuce/$FINAL_KERNEL_ZIP
